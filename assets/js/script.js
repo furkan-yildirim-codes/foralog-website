fd5b746361d7deb1cf89a3a336a443edd33c8a39
Furkan Yildirim <fyildirim8@stu.okan.edu.tr>
2026-03-01 15:15:21 +0300

    Repair Hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,5 @@ const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
-    if (navMenu.style.display === "flex") {
-        navMenu.style.display = "none";
-    } else {
-        navMenu.style.display = "flex";
-    }
+    navMenu.classList.toggle("active");
 });
